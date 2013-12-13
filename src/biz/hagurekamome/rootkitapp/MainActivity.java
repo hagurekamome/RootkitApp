11e8189f50062519cc1018aba64c0c5109489258
hagurekamome <seagull0130@gmail.com>
2013-12-13 17:33:36 +0900

    javaで置き換えたためinstall_tool.sh削除

--- src/biz/hagurekamome/rootkitapp/MainActivity.java
+++ src/biz/hagurekamome/rootkitapp/MainActivity.java
@@ -1,11 +1,14 @@
 package biz.hagurekamome.rootkitapp;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -55,7 +58,9 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				
-				new AsyncAppTask().execute();
+				AsyncAppTask task = new AsyncAppTask();
+				task.execute();
+				task = null;
 
 			}
 			
@@ -68,61 +73,16 @@ public class MainActivity extends Activity {
 
 				result = getRoot(prepareKernelCred, commitCreds, ptmxFops);
 
-				if(result){
-					msgView.append("\nSucceedes get temporary root.\n\n");
-				
-					File suFile = writeAssetToCacheFile("su", 00644);
-					if(suFile == null){
-						msgView.setText("su file not found");
-						return;
-					}
+				if(!result)
+					return;
 
-					File busyboxFile = writeAssetToCacheFile("busybox", 00755);
-					if(busyboxFile == null){
-						msgView.setText("busybox file not found");
-						return;
-					}
-
-					File ricFile = writeAssetToCacheFile("00stop_ric", 00644);
-					if(ricFile == null){
-						msgView.setText("00stopric file not found");
-						return;
-					}
-
-					File apkFile = writeAssetToCacheFile("Superuser.apk", 00644);
-					if(apkFile == null){
-						msgView.setText("SuperSU file not found");
-						return;
-					}
-
-				
-					String str_result;
-				
-					msgView.append("Cpoy Files...\n\n");
-
-					str_result = executeScript("install_tool.sh");
-
-					msgView.append(str_result + "\n\n");
-
-					if(scriptResult == 0){
-						msgView.append("Succeeded get root!!\n\n");
-						msgView.append("Please Reboot.");
-						rebootButton.setEnabled(true);
-					}else{
-						msgView.append("Succeeded get the temporary root.\n\n");
-						msgView.append("But Script Error.\n\n");
-						msgView.append("Error Code=" + Integer.toString(scriptResult));
-						msgView.append("\nPlease Check\n/data/local/tmp/err.txt");
-					}
-
-					suFile.delete();
-					busyboxFile.delete();
-					ricFile.delete();
-					apkFile.delete();
-				}
-				
+				if(!installTool())
+					return;
+					
+				msgView.append("Succeeded get root!!\n\n");
+				msgView.append("Please Reboot.");
+				rebootButton.setEnabled(true);
 			}
-			
 		});
 
 		rebootButton.setOnClickListener(new OnClickListener() {
@@ -195,18 +155,9 @@ public class MainActivity extends Activity {
 		}
 	}
 
-
-	private String executeScript(String name) {
-		File scriptFile = writeAssetToCacheFile(name, 00755);
-		if (scriptFile == null)
-			return "Could not find asset \"" + name + "\"";
-
+	private String executeCommand(String [] strCommand) {
 		try {
-			Process p = Runtime.getRuntime().exec(
-					new String [] {
-						scriptFile.getAbsolutePath(),
-						" 2>&1"
-					});
+			Process p = Runtime.getRuntime().exec(strCommand);
 			BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 			StringBuilder sb = new StringBuilder();
@@ -234,12 +185,9 @@ public class MainActivity extends Activity {
 			StringWriter sw = new StringWriter();
 			e.printStackTrace(new PrintWriter(sw));
 			return sw.toString();
-		} finally {
-			scriptFile.delete();
 		}
 	}
 
-	
 	private String executeReboot() {
 		try {
 			Process p = Runtime.getRuntime().exec("reboot");
@@ -291,9 +239,9 @@ public class MainActivity extends Activity {
 				commitCreds = addr[1];
 				ptmxFops = addr[2];
 				msgView.append("Get Address Success\n\n");
-				msgView.append("0x" + Long.toHexString(prepareKernelCred)+ ": prepare_kernel_cred\n\n" );
-				msgView.append("0x" + Long.toHexString(commitCreds) + ": commit_creds\n\n");
-				msgView.append("0x" + Long.toHexString(ptmxFops) + ": ptmx_fops\n\n");
+				msgView.append("0x" + Long.toHexString(prepareKernelCred)+ " : prepare_kernel_cred\n" );
+				msgView.append("0x" + Long.toHexString(commitCreds) + " : commit_creds\n");
+				msgView.append("0x" + Long.toHexString(ptmxFops) + " : ptmx_fops\n\n");
 				msgView.append("Press GetRoot Button\n");
 
 				getrootButton.setEnabled(true);
@@ -320,4 +268,199 @@ public class MainActivity extends Activity {
 		
 	}
 
+	private boolean copyFileAndSetPermission(File src, File dist, int mode){
+
+		if(!FileUtils.copyFile(src, dist))
+			return false;
+
+		if(FileUtils.setPermissions(dist.getAbsolutePath(), mode, -1, -1) != 0)
+			return false;
+
+		return true;
+	}
+
+	private boolean installTool(){
+		String str_result;
+
+		msgView.append("\nSucceedes get temporary root.\n\n");
+		
+		File suFile = writeAssetToCacheFile("su", 00644);
+		if(suFile == null){
+			msgView.append("su file not found");
+			return false;
+		}
+
+		File busyboxFile = writeAssetToCacheFile("busybox", 00755);
+		if(busyboxFile == null){
+			msgView.append("busybox file not found");
+			return false;
+		}
+
+		File ricFile = writeAssetToCacheFile("00stop_ric", 00644);
+		if(ricFile == null){
+			msgView.append("00stopric file not found");
+			return false;
+		}
+
+		File apkFile = writeAssetToCacheFile("Superuser.apk", 00644);
+		if(apkFile == null){
+			msgView.append("SuperSU file not found");
+			return false;
+		}
+
+		//Disable ric
+		int ricType = 0;
+
+		if(new File("/sbin/ric").exists()){
+			msgView.append("Disable ric...");
+			str_result = executeCommand(new String [] {busyboxFile.getAbsolutePath(), "pkill", "/sbin/ric"});
+			msgView.append(str_result);
+			if(!reMount("/", "rw"))
+				return false;
+			str_result = executeCommand(new String [] {"/system/bin/rm", "/sbin/ric"});
+			msgView.append(str_result);
+			if(!reMount("/", "ro"))
+				return false;
+			str_result = executeCommand(new String [] {busyboxFile.getAbsolutePath(), "pkill", "/sbin/ric"});
+			msgView.append(str_result);
+			msgView.append("OK\n\n");
+			ricType = 1;
+		}
+		if(new File("/systen/bin/ric").exists()){
+			msgView.append("Disable ric...");
+			str_result = executeCommand(new String [] {"stop", "ric", "2>&1"});
+			msgView.append(str_result);
+			if(!reMount("/system", "rw"))
+				return false;
+			str_result = executeCommand(new String [] {"rm", "/system/bin/ric", "2>&1"});
+			msgView.append(str_result);
+			if(!reMount("/system", "ro"))
+				return false;
+			msgView.append("OK\n\n");
+			ricType = 2;
+		}
+
+		//remount system
+		if(!reMount("/system", "rw"))
+			return false;
+
+		//Cpoy su
+		msgView.append("Copy su and set permission...");
+		if(!copyFileAndSetPermission(suFile, new File("/system/xbin/su"), 06755)){
+			msgView.append("su file can't copy /system/xbin\n\n");
+			return false;
+		}
+		msgView.append("OK\n\n");
+		
+		msgView.append("Make symbolic link su...");
+		str_result =executeCommand(new String [] {"/system/bin/ln", "-s", "/system/xbin/su", "/system/bin/su"});
+		if(str_result != ""){
+			if(str_result.indexOf("File exists") == 0 )
+				msgView.append("\n" + str_result +"\n");
+		}
+		msgView.append("OK\n\n");
+
+		//Cpoy SuperSU
+		msgView.append("Copy Superuser.apk and set permission...");
+		if(!copyFileAndSetPermission(apkFile, new File("/system/app/Superuser.apk"), 00644)){
+			msgView.append("SuperSU file can't copy.");
+			return false;
+		}
+		msgView.append("OK\n\n");
+
+		//Copy busybox
+		msgView.append("Copy busybox and set permission...");
+		if(!copyFileAndSetPermission(busyboxFile, new File("/system/xbin/busybox"), 00755)){
+			msgView.append("busybox file can't copy.");
+			return false;
+		}
+		msgView.append("OK\n\n");
+		str_result =executeCommand(new String [] {"/system/bin/chown", "root.shell", "/system/xbin/busybox"});
+		if(str_result != ""){
+			msgView.append("\n" + str_result +"\n");
+			return false;
+		}
+
+		//Install busybox
+		msgView.append("Install busybox...");
+		str_result =executeCommand(new String [] {"/system/xbin/busybox", "--install", "-s", "/system/xbin"});
+		if(str_result != ""){
+			msgView.append("\n" + str_result +"\n");
+			return false;
+		}
+		msgView.append("OK\n\n");
+		
+		if(ricType == 1){
+			msgView.append("Make init.d directory...");
+			File initD = new File("/system/etc/init.d");
+			if(!initD.exists()){
+				if(!initD.mkdir()){
+					msgView.append("Can't create /system/etc/init.d\n");
+					return false;
+				}
+			}
+			if(FileUtils.setPermissions(initD.getAbsolutePath(), 00755, -1, -1) != 0){
+				msgView.append("Can't change permission /system/eyc/init.d\n");
+				return false;
+			}
+			msgView.append("OK\n\n");
+			
+			if(!copyFileAndSetPermission(new File("/system/etc/hw_config.sh"), new File("/data/local/tmp/hw_config.sh.org"), 00644)){
+				msgView.append("Backup error hw_config.sh.\n" );
+				return false;
+			}
+			msgView.append("Modefy /system/etc/hw_config...");
+			try{
+				String enableInitd = "/system/xbin/busybox run-parts /system/etc/init.d\n";
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/system/etc/hw_config.sh", true), "UTF-8"));
+				bw.append(enableInitd);
+				bw.close();
+			}catch (FileNotFoundException e){
+				msgView.append(e.toString());
+				return false;
+			}catch (IOException e){
+				msgView.append(e.toString());
+				return false;
+			}
+			msgView.append("OK\n\n");
+		}
+		
+		//Copy 00stop_ric
+		if(ricType == 1){
+			msgView.append("Copy 00stop_ric and set permission...");
+			if(!copyFileAndSetPermission(ricFile, new File("/system/etc/init.d/00stopo_ric"), 00755)){
+				msgView.append("busybox file can't copy.");
+				return false;
+			}
+			msgView.append("OK\n\n");
+		}
+		
+		//remount /system
+		if(!reMount("/system", "ro"))
+			return false;
+
+		suFile.delete();
+		busyboxFile.delete();
+		ricFile.delete();
+		apkFile.delete();
+		
+		return true;
+	}
+
+	private boolean reMount(String strDir, String mode){
+		String str_result = executeCommand(new String [] {"/system/bin/mount", "-o", "remount," + mode, strDir});
+		if(mode == "ro"){
+			if(str_result.indexOf("Device or resource busy") != 0)
+				return true;
+			else {
+				msgView.append("");
+				return false;
+			}
+		}
+		if(str_result == "")
+			return true;
+
+		msgView.append(str_result);
+		return false;
+	}
 }
